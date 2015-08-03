54b361e69aab338625af5d641dbc78da3638e3e5
Charles Vaillancourt <umbsublime@gmail.com>
2015-08-03 17:57:17 -0400

    cleaned up bw-counter.py

--- bw-counter.py
+++ bw-counter.py
@@ -1,31 +1,10 @@
 #!/usr/bin/env python
 
-from core import chains
-from core import rules, rule_set
-
-def print_counter(rule_group):
-    all_counters = {}
-    all_rules = rule_group.get_rules()
-    in_rules = all_rules['Input']
-    out_rules = all_rules['Output']
-    in_out_rules = all_rules['In_Out']
-
-    for chain_name in rule_group.get_chain_names():
-        all_counters[chain_name] = {'rules': [],
-                                   'counters': rule_group.get_chain_counter(chain_name)}
-        if 'in_out' in chain_name:
-            all_counters[chain_name]['rules'] = in_out_rules
-        elif 'in' in chain_name:
-            all_counters[chain_name]['rules'] = in_rules
-        elif 'out' in chain_name:
-            all_counters[chain_name]['rules'] = out_rules
-
-
-    from pprint import pprint
-    pprint (all_counters)
-
+from core import chains, rules, counters
 
 def main():
+    from pprint import pprint
+
     print ':) main brah'
 
     dicom_in = rules.InputRule('DICOM In',protocol= 'tcp', sport=5000)
@@ -33,28 +12,14 @@ def main():
     http_in = rules.InOutRule('HTTP In', sport=80)
     http_out = rules.InOutRule('HTTP Out', dport=80)
 
-    dicom_rules = {'Input': [dicom_in],
-                   'Output': [dicom_out],
-                   'In_Out': [http_out, http_in]}
-    rule_group = rule_set.RuleGroup('TEST',dicom_rules)
+    dicom_rules = [dicom_in, dicom_out, http_out, http_in]
+    dicom_counter = counters.Counter('TEST',dicom_rules)
 
     ssh = rules.InOutRule('SSH',dport=22)
-    rule_group.add_rule(ssh)
-
-    #rule_group.delete_rule(ssh)
-    #rule_group.delete_rule(http_in)
-    #rule_group.delete_rule(http_out)
-    #rule_group.delete_rule(dicom_in)
-    #rule_group.delete_rule(dicom_out)
+    dicom_counter.add_rule(ssh)
+    pprint(dicom_counter.get_counters())
 
 
-
-    from time import sleep
-    while True:
-        print '--------------'
-        #print_counter(rule_group)
-        print chains.get_chain_counters('TEST_in_out')
-        sleep(5)
 
 if __name__ == '__main__':
     main()
